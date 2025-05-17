01c0f6df77b84d122ab5fcb838152e79bd04eee2
maciekwiso <maciekwiso@o2.pl>
2025-05-17 20:38:54 +0200

    updated config

--- pushcfgh1.sh
+++ pushcfgh1.sh
@@ -11,6 +11,10 @@ cp -r init.lua lazy-lock.json lazyvim.json stylua.toml lua ${me}/nvim/lazy
 rm -r ${me}/qmk/keychron-k15max/*
 cp -r /mnt/d/Dev/qmk_firmware/keyboards/keychron/k15_max/ansi_encoder/white/keymaps/ ${me}/qmk/keychron-k15max
 
+#intellij
+rm -r ${me}/intellij/*
+cp -r /mnt/c/Users/macie/AppData/Roaming/JetBrains/IdeaIC2024.3/keymaps ${me}/intellij/*
+
 #push to git
 cd $me
 git add .
