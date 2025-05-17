69f2f4cbbdfaa7fdc2608da491ac444ccdee89aa
maciekwiso <maciekwiso@o2.pl>
2025-05-17 20:28:26 +0200

    updated config

--- pushcfgh1.sh
+++ pushcfgh1.sh
@@ -4,9 +4,11 @@ me=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 #nvim lazy
 cd ~/.config/nvim/
+rm -r ${me}/nvim/lazy/*
 cp -r init.lua lazy-lock.json lazyvim.json stylua.toml lua ${me}/nvim/lazy
 
 #qmk
+rm -r ${me}/qmk/keychron-k15max/*
 cp -r /mnt/d/Dev/qmk_firmware/keyboards/keychron/k15_max/ansi_encoder/white/keymaps/ ${me}/qmk/keychron-k15max
 
 #push to git
