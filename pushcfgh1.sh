740e21ece3aa1b676b670873998381a3a7f41acc
maciekwiso <maciekwiso@o2.pl>
2025-05-17 20:45:44 +0200

    updated config

--- pushcfgh1.sh
+++ pushcfgh1.sh
@@ -12,8 +12,9 @@ rm -r ${me}/qmk/keychron-k15max/*
 cp -r /mnt/d/Dev/qmk_firmware/keyboards/keychron/k15_max/ansi_encoder/white/keymaps/ ${me}/qmk/keychron-k15max
 
 #intellij
-rm -r ${me}/intellij/*
+rm -r ${me}/intellij/keymaps/*
 cp -r /mnt/c/Users/macie/AppData/Roaming/JetBrains/IdeaIC2024.3/keymaps ${me}/intellij
+cp /mnt/c/Users/macie/.ideavimrc ${me}/intellij
 
 #push to git
 cd $me
