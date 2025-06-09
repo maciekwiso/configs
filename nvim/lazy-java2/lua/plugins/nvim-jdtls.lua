0026a967b802cbe45d8480478d9810db4c73f056
maciekwiso <maciekwiso@o2.pl>
2025-06-10 00:17:19 +0200

    updated config

--- nvim/lazy-java2/lua/plugins/nvim-jdtls.lua
+++ nvim/lazy-java2/lua/plugins/nvim-jdtls.lua
@@ -1,1 +1,26 @@
-return { "mfussenegger/nvim-jdtls", dependencies = "hrsh7th/cmp-nvim-lsp" }
+return {
+  "mfussenegger/nvim-jdtls",
+  dependencies = "hrsh7th/cmp-nvim-lsp",
+  opts = {
+    jdtls = function(opts)
+      table.insert(opts.cmd, "--java-executable=/usr/lib/jvm/java-1.21.0-openjdk-amd64/bin/java")
+      opts.settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-17",
+                path = "/opt/jdk-17.0.12",
+              },
+              {
+                name = "JavaSE-21",
+                path = "/usr/lib/jvm/java-1.21.0-openjdk-amd64",
+              },
+            },
+          },
+        },
+      }
+      return opts
+    end,
+  },
+}
--- wsl/.bashrc
+++ wsl/.bashrc
@@ -102,6 +102,6 @@ export JAVA_HOME="/usr/lib/jvm/java-1.21.0-openjdk-amd64"
 alias pucfg=~/workspace/configs/pushcfgh1.sh
 alias v=nvim
 alias vv='nvim -c ":term" -c startinsert'
-
+alias j17='export JAVA_HOME=/opt/jdk-17.0.12; export PATH=${JAVA_HOME}/bin:${PATH}'
 # Created by `pipx` on 2025-05-30 19:29:26
 export PATH="$PATH:/root/.local/bin"
