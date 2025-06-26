3eb88d32ec8479a727a5c86b5eb7f96aff40fff2
maciekwiso <maciekwiso@o2.pl>
2025-06-26 23:58:24 +0200

    updated config

--- nvim/lazy-java2/lua/config/autocmds.lua
+++ nvim/lazy-java2/lua/config/autocmds.lua
@@ -6,3 +6,22 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local search_regs = {}
+
+local function search_bufleave()
+  search_regs[vim.fn.bufname()] = vim.fn.getreg("/")
+end
+
+local function search_bufenter()
+  vim.fn.setreg("/", search_regs[vim.fn.bufname()])
+end
+
+vim.api.nvim_create_autocmd({ "BufLeave" }, {
+  pattern = "*",
+  callback = search_bufleave,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = "*",
+  callback = search_bufenter,
+})
