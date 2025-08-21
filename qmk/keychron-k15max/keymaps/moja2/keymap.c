79c3fa7712c9f603428d06eedb374f95a7643bdd
maciekwiso <maciekwiso@o2.pl>
2025-08-21 18:53:34 +0200

    updated config

--- qmk/keychron-k15max/keymaps/moja2/keymap.c
+++ qmk/keychron-k15max/keymaps/moja2/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,     KC_F8,    KC_F9,   KC_F10,   KC_F11,   TO(GAME1),_______,            _______,
         _______,   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,     KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,            _______,
         _______,   _______,  _______,  A(KC_TAB),KC_UP,C(S(KC_TAB)), C(KC_TAB),XXXXXXX,  KC_7,      KC_8,     KC_9,    XXXXXXX,  _______,  _______,  _______,            _______,
-        _______,   KC_CAPS,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  XXXXXXX,  KC_4,      KC_5,     KC_6,    XXXXXXX,  _______,            _______,            _______,
+        _______,   KC_CAPS,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_ENT,   XXXXXXX,  KC_4,      KC_5,     KC_6,    XXXXXXX,  _______,            _______,            _______,
         _______,   _______,            XXXXXXX,  XXXXXXX,  KC_HOME,  KC_END,   XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_1,    KC_2,     KC_3,     XXXXXXX,  _______,    _______,
         _______,   _______,  _______,            _______,  _______,  _______,                       KC_0,              XXXXXXX,  _______,            _______,    _______,  _______),
         
