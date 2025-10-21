dad954f26afa2bfad36f7623706241d35703ded0
maciekwiso <maciekwiso@o2.pl>
2025-10-21 19:46:52 +0200

    updated config

--- qmk/keychron-k15max/ansi_encoder/white/keymaps/default/keymap.c
+++ qmk/keychron-k15max/ansi_encoder/white/keymaps/default/keymap.c
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_FN] = LAYOUT_ansi_90(/* right FN */
         BL_TOGG,   _______,  BL_DOWN,  BL_UP,    KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            _______,
         _______,   A(KC_F4), KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
-        _______,   KC_TAB,   S(KC_6),  S(KC_BSLS),S(KC_7), S(KC_MINS),S(KC_4), KC_PGUP,  KC_PGDN,  OSM(MOD_LSFT),  TLDSL,    SPRL,     _______,  _______,  _______,            KC_PGDN,
+        _______,   KC_TAB,   S(KC_6),  S(KC_BSLS),S(KC_7), S(KC_MINS),S(KC_4), KC_PGUP,  KC_PGDN,  OSM(MOD_LSFT),TLDSL,SPRL,     _______,  _______,  _______,            KC_PGDN,
         _______,   KC_CAPS,  KC_EQUAL, S(KC_1),  HM2_D,    HM2_F,    KC_MINS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  S(KC_SCLN),S(KC_QUOT),        _______,            KC_END,
         _______,   _______,            S(KC_3),  S(KC_2),  S(KC_5),  S(KC_8),  S(KC_EQL),BAT_LVL,  KC_HOME,  KC_END,   S(KC_COMM),S(KC_DOT),KC_BSLS, _______,    _______,
         _______,   _______,  _______,            _______,  _______,  _______,                      _______,            _______,  _______,             _______,   _______,  _______),
@@ -127,8 +127,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,     KC_F8,    KC_F9,   KC_F10,   KC_F11,   TO(GAME1),_______,            _______,
         _______,   _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,     KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,            _______,
         _______,   _______,  KC_HOME,  KC_UP,    KC_END,   KC_PGUP,  KC_PGDN,  XXXXXXX,  KC_7,      KC_8,     KC_9,    XXXXXXX,  _______,  _______,  _______,            _______,
-        _______,   KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_ENT,   XXXXXXX,  XXXXXXX,  KC_4,      KC_5,     KC_6,    XXXXXXX,  _______,            _______,            _______,
-        _______,   _______,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  KC_1,    KC_2,     KC_3,     XXXXXXX,  _______,    _______,
+        _______,   KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_ENT,   KC_MINS,  XXXXXXX,  KC_4,      KC_5,     KC_6,    XXXXXXX,  _______,            _______,            _______,
+        _______,   _______,            S(KC_3),  S(KC_2),  S(KC_5),  S(KC_8),  S(KC_EQL),XXXXXXX,   XXXXXXX,  KC_1,    KC_2,     KC_3,     XXXXXXX,  _______,    _______,
         _______,   _______,  _______,            _______,  _______,  _______,                       KC_0,              XXXXXXX,  _______,            _______,    _______,  _______),
         
     [MOD3] = LAYOUT_ansi_90(/* left space hold */
