e4487dac17691e6e7da0d68496724462db8a6b61
maciekwiso <maciekwiso@o2.pl>
2026-08-26 15:09:38 +0200

    Update keymap.c

--- qmk/keychron-k15max/ansi_encoder/white/keymaps/default/keymap.c
+++ qmk/keychron-k15max/ansi_encoder/white/keymaps/default/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______,  KC_F1,    KC_F2,       KC_F3,    KC_F4,     KC_F5,      KC_F6,      KC_F7,     KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,            _______,
         _______,   _______,  KC_LSFT,  KC_LCTL,     KC_HOME,  KC_END,    KC_PGUP,    XXXXXXX,    KC_7,      KC_8,     KC_9,    XXXXXXX,  _______,  _______,  _______,            _______,
         _______,   KC_CAPS,  A(KC_TAB),C(S(KC_TAB)),C(KC_TAB),KC_ENT,    KC_PGDN,    XXXXXXX,    KC_4,      KC_5,     KC_6,    XXXXXXX,  _______,            _______,            _______,
-        _______,   _______,            MS_BTN1,     MS_BTN2,  KC_F5,     MS_BTN4,    MS_BTN5,    XXXXXXX,   XXXXXXX,  KC_1,    KC_2,     KC_3,     XXXXXXX,  _______,    _______,
+        _______,   _______,            C(G(KC_LEFT)),C(G(KC_RIGHT)),  KC_F5,     MS_BTN4,    MS_BTN5,    XXXXXXX,   XXXXXXX,  KC_1,    KC_2,     KC_3,     XXXXXXX,  _______,    _______,
         _______,   _______,  _______,               _______,  _______,   _______,                         KC_0,              XXXXXXX,  _______,            _______,    _______,  _______),
 
     [MOD2] = LAYOUT_ansi_90(/* A hold */
